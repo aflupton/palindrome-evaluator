00f26b7a8608480aacb34630e0c368eca4e11e91
andrew lupton and Eva Antipina <andrew@>
2018-03-27 15:47:16 -0700

    add initial files and back-end logic

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var ifItPolindrome = function(arr){
+    if (arr.length % 2 != 0){
+      var middle = arr[arr.length/2].split("");
+      if (middle.length % 2 != 0){
+        return -1;
+      } else {
+        for (var i = 0; i < middle.length/2; i++){
+          if (middle[i] != middle[arr.length -i]){
+            return -1;
+          };
+        };
+      } ;
+      for (var i = 0; i < arr.length/2; i++){
+        var temp = arr[(arr.length - 1) - i].split('').reverse().join('');
+
+        if (arr[i] != temp){
+            return -1;
+        };
+      };
+    } else {
+      for (var i = 0; i < arr.length/2; i++){
+        var temp = arr[(arr.length - 1) - i].split('').reverse().join('');
+
+        if (arr[i] != temp){
+            return -1;
+        };
+      };
+      return 1;
+  }
+};
+
+// UI logic
